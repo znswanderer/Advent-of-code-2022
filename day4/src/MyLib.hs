457e7bc11d1209d5766de2987078ade720f5d261
Nina Scheffler <znswanderer@icloud.com>
2022-12-04 15:35:27 +0100

    Update MyLib.hs

--- day4/src/MyLib.hs
+++ day4/src/MyLib.hs
@@ -1,17 +1,20 @@
 module MyLib (sumFullOverlaps, sumPartialOverlaps) where
 
 import Text.Parsec.String (Parser)
-import Text.Parsec.Token (integer)
 import Text.Parsec.Char (string, char, digit, oneOf, endOfLine)
 import Text.Parsec.Combinator (many1, sepBy)
 import Text.Parsec (parse, ParseError, eof)
 
-import Data.Set (Set, intersection, elemAt, isSubsetOf)
+import Data.Set (Set, intersection, isSubsetOf)
 import qualified Data.Set as Set
 
 {-
 
-OK, it's probably a bit over the top at the moment, but I'm going to start using parsec now, as recommended in https://wjwh.eu/posts/2022-11-30-haskell-aoc-tricks.html. It's probably good to gain experience with parsec when the problems are still relatively simple.
+OK, it's probably a bit over the top at the moment, but I'm going to start 
+using parsec now, as recommended in
+https://wjwh.eu/posts/2022-11-30-haskell-aoc-tricks.html. It's probably 
+good to gain experience with parsec when the problems are still relatively
+simple.
 
 -}
 
