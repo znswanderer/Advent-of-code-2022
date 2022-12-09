7c22d073b4568289c8d9531031b71b0c4097bd9d
Nina Scheffler <znswanderer@icloud.com>
2022-12-09 16:22:32 +0100

    Update MyLib.hs

--- day9/src/MyLib.hs
+++ day9/src/MyLib.hs
@@ -12,12 +12,14 @@ type Rope = [Pos]
 data Move = MUp | MDown | MRight | MLeft
     deriving (Eq, Show)
 
-mright n = replicate n MRight
-mleft n = replicate n MLeft
-mup n = replicate n MUp
-mdown n = replicate n MDown
 
-example = concat [mright 4, mup 4, mleft 3, mdown 1, mright 4, mdown 1, mleft 5, mright 2]
+buildMoves :: String -> [Move]
+buildMoves = concat . map buildMoves' . lines
+  where
+    buildMoves' ('R':s) = replicate (read s) MRight
+    buildMoves' ('L':s) = replicate (read s) MLeft
+    buildMoves' ('U':s) = replicate (read s) MUp
+    buildMoves' ('D':s) = replicate (read s) MDown
 
 
 movePos :: Pos -> Move -> Pos
@@ -33,41 +35,35 @@ follow (hx, hy) t@(tx, ty) =
         dy = hy - ty
     in
         if abs dx > 1 || abs dy > 1 then (tx + pmOne dx, ty + pmOne dy) else t
-
--- Constrict to interval [-1, 1]
-pmOne :: Int -> Int
-pmOne = (max (-1)) . (min 1) 
-
-makeMoves :: (a -> Move -> a) -> a -> [Move] -> [a]
-makeMoves = scanl'
-
-
-buildMoves :: String -> [Move]
-buildMoves = concat . map buildMoves' . lines
   where
-    buildMoves' :: String -> [Move]
-    buildMoves' ('R':s) = mright $ read s
-    buildMoves' ('L':s) = mleft $ read s
-    buildMoves' ('U':s) = mup $ read s
-    buildMoves' ('D':s) = mdown $ read s
+    -- Constrict to interval [-1, 1]
+    pmOne :: Int -> Int
+    pmOne = (max (-1)) . (min 1) 
+
 
 -- foldl' :: (b -> a -> b) -> b -> t a -> b 
 moveRope :: Rope -> Move -> Rope
 moveRope (p:ps) mv = 
     reverse $ foldl' (\hs p -> (follow (head hs) p):hs) [movePos p mv] ps
 
+makeMoves :: (a -> Move -> a) -> a -> [Move] -> [a]
+makeMoves = scanl'
+
 numVisited :: Rope -> [Move] -> Int
 numVisited rp = length . Set.fromList  . (map (head . reverse)) . (makeMoves moveRope rp)
 
 
+rope2 :: [(Int, Int)]
 rope2 = replicate 2 (0,0)
 
+part1 :: String -> Int
 part1 = (numVisited rope2) . buildMoves
 
 
 -- ---------------- PART 2 -------------------
 
+rope10 :: [(Int, Int)]
 rope10 = replicate 10 (0,0)
 
-
+part2 :: String -> Int
 part2 = (numVisited rope10) . buildMoves
