880f0a3349c78a703a48862583b31f41472aa09b
Nina Scheffler <znswanderer@icloud.com>
2022-12-10 15:33:25 +0100

    Update MyLib.hs

--- day10/src/MyLib.hs
+++ day10/src/MyLib.hs
@@ -7,41 +7,6 @@ import System.IO.Unsafe (unsafePerformIO)
 -- Getting the text files for interactive ghci usage...
 unsafeReadFile = unsafePerformIO . readFile 
 
-{-
--- State Machine
-
-
-type CycleCounter = Int
-
-data Operation = NOOP | ADDX Int
-
-type OperationCounter = Int
-
-type Instruction = (Operation, OperationCounter)
-
-type Register = Int
-
-type CPUState = (CycleCounter, Register)
-
-exec :: CPUState -> Instruction -> [CPUState]
-exec (cc, rg) (NOOP, _)   = (cc+1, rg)  :[]
-exec (cc, rg) (ADDX n, 1) = (cc+1, rg)  :(exec (cc+1, rg) (ADDX n, 0))
-exec (cc, rg) (ADDX n, 0) = (cc+1, rg+n):[]
-
-instruction :: Operation -> Instruction
-instruction NOOP     = (NOOP, 0)
-instruction (ADDX n) = (ADDX n, 1)
-
-example = [NOOP, ADDX 3, ADDX (-5)]
-
---run :: CPUState -> [Instruction] -> [CPUState]
---run 
-
-runEx ops = foldM exec (0, 0) (reverse (map instruction ops))
-
-
-
--}
 
 data Pseudo = NewCycle | Incr Int deriving (Eq, Show)
 
@@ -71,14 +36,6 @@ readProgam :: String -> [Pseudo]
 readProgam = concat . (map readLine) . lines 
 
 runProgram = scanl' run start
-
-{-
-every :: (Int -> Bool) -> [CPU] -> [CPU]
-every pred = filter f 
-  where
-    f (CC c, r) = pred c 
-
--}
 
 focusCycles :: [Register] -> [(Int, Register)]
 focusCycles rs = filter (\(x, _) -> (x - 20) `mod` 40 == 0) $ zip [1..] rs
