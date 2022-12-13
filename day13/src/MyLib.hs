c3db583bf4450ddd7216e6c312cf413ba64969ef
Nina Scheffler <znswanderer@icloud.com>
2022-12-13 15:21:38 +0100

    haskell version of part1

--- day13/src/MyLib.hs
+++ day13/src/MyLib.hs
@@ -1,37 +1,75 @@
- {-# LANGUAGE FlexibleInstances #-}
- {-# LANGUAGE MultiParamTypeClasses #-}
- 
 module MyLib (part1, part2) where
 
+import Text.Parsec.String (Parser)
+import Text.Parsec.Char (anyChar, string, char, digit, oneOf, endOfLine, letter, spaces, alphaNum, noneOf, satisfy)
+import Text.Parsec.Combinator (many1, sepBy1, endBy, lookAhead, sepBy)
+import Text.Parsec (parse, ParseError, eof, (<|>), skipMany, manyTill, try, many)
+
+import System.IO.Unsafe (unsafePerformIO)
+-- Getting the text files for interactive ghci usage...
+unsafeReadFile = unsafePerformIO . readFile 
+
+regularParse :: Parser a -> String -> Either ParseError a
+regularParse p = parse p ""
+
+num :: Parser Int
+num = do
+    n <- many1 digit
+    return (read n)
 
 
-{-
-class ElfComp a  b where
-    ecomp :: a -> b -> Bool
+data EMsg = EInt Int | EList [EMsg]
+    deriving (Eq, Show)
 
-instance (ElfComp a b) => (ElfComp [a] [b]) where
-    ecomp xs ys =
-        if (length xs) > (length ys) 
-            then False
-            else and $ map (uncurry ecomp) (zip xs ys)
+x = [EInt 1, EInt 2, EList [EInt 2, EInt 3]]
 
-instance ElfComp Integer Integer where
-    ecomp a b = a <= b
+elfie :: EMsg -> EMsg -> Ordering
+elfie (EInt l)        (EInt r)       = compare l r
+elfie (EList [])      (EList [])     = EQ
+elfie (EList [])      (EList _)      = LT
+elfie (EList _)       (EList [])     = GT
+elfie (EList (l:ls))  (EList (r:rs)) = 
+    let pos1 = elfie l r
+    in if pos1 == EQ then elfie (EList ls) (EList rs) else pos1
+elfie (EInt l)        r@(EList _)    = elfie (EList [EInt l]) r
+elfie l@(EList _)     (EInt r)       = elfie l (EList [EInt r])
 
-instance (ElfComp a b) => (ElfComp a [b]) where
-    ecomp x ys = ecomp [x] ys
+eVal :: Parser EMsg
+eVal = eInt <|> eList
+    
+eInt :: Parser EMsg
+eInt = do
+    n <- num
+    return $ EInt n
 
--}
+eList :: Parser EMsg
+eList = do
+    char '['
+    vals <- sepBy eVal (char ',')
+    char ']'
+    return $ EList vals
 
-class ElfComb a where
-    ---
+eCompare :: String -> String -> Ordering
+eCompare l r = 
+    let 
+        Right l' = regularParse eList l
+        Right r' = regularParse eList r
+    in
+        elfie l' r'
 
-ecompInt :: Int -> Int -> Bool
-ecompInt a b = a <= b
+readEList :: Parser [EMsg]
+readEList = sepBy eList spaces
 
-ecompIntList :: [Int] -> 
+runList :: [EMsg] -> [Ordering]
+runList [] = []
+runList (l:r:xs) = (elfie l r):runList xs
 
 
-part1 = const "part1"
+allValidIndices :: [EMsg] -> [Int]
+allValidIndices xs = map fst $ filter (\(n, x) -> x == LT) $ zip [1..] $ runList xs
+
+part1 s = 
+    let Right xs = regularParse readEList s
+    in sum $ allValidIndices xs
 
 part2 = const "part2"
